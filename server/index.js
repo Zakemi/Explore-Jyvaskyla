23155e06f1bba5e0ffd492e1536e7074a7ea790e
Gálffy Tamás <ezittgtx@gmail.com>
2016-10-25 21:24:29 +0300

    Locations now queried from MySQL database

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+var sql = mysql.createPool({
+    host: 'localhost',
+    user: 'jyvaskyla',
+    password: 'jyvaskyla',
+    database: 'jyvaskyla'
+    // jyvaskyla: 'jyvaskyla'
+});
 
 var PORT = 3000;
 
-app.get('/', function(req, res) {
-    res.send([
-        '<html>',
-        '   <head></head>',
-        '   <body>Hello world!</body>',
-        '</html>'
-    ].join('\n'));
+app.get('/locations', function(req, res) {
+    sql.query('SELECT * FROM locations', function(err, rows, fields) {
+        if(err) {
+            console.log('Locations query fail!', err);
+            return;
+        }
+
+        res.send(JSON.stringify(rows));
+    });
 });
 
 app.listen(PORT, function() {
-    console.log('Listening on port ', PORT);
+    console.log('Listening on port', PORT);
 });
