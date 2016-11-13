f7454072c140f0426a5ac9cc8c20f62dee81f495
Gálffy Tamás <ezittgtx@gmail.com>
2016-11-13 21:20:48 +0200

    Can now POST to /locations

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+
 var sql = mysql.createPool({
     host: 'localhost',
     user: 'jyvaskyla',
@@ -15,10 +16,29 @@ app.get('/locations', function(req, res) {
     sql.query('SELECT * FROM locations', function(err, rows, fields) {
         if(err) {
             console.log('Locations query fail!', err);
+            res.statusCode = 500;
+            res.send('Query fail!');
             return;
         }
 
         res.send(JSON.stringify(rows));
+    });
+});
+
+app.post('/locations', function(req, res) {
+    var body = [];
+
+    req.on('error', function(err) {
+        console.log(err);
+    }).on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+        body = Buffer.concat(body).toString();
+
+        // Handle request
+        json = JSON.parse(body);
+        res.send(JSON.stringify(json));
+        console.log(json);
     });
 });
 
