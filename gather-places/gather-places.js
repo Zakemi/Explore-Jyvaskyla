f335ce1e7c8bbb1fed57575d50ff0cdb87746216
Gálffy Tamás <ezittgtx@gmail.com>
2016-11-15 00:25:05 +0200

    Data downladed from server

--- gather-places/gather-places.js
+++ gather-places/gather-places.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var https = require('https');
 
-var API_KEY = "AIzaSyDd3X275fD1ZBotmfU52iDTPSmfAjaZO9M";
+const API_KEY = "AIzaSyDd3X275fD1ZBotmfU52iDTPSmfAjaZO9M";
+
+// Middle of Jyvaskyla ( according to Google Maps )
 var Latitude = 62.1367014;
 var Longitude = 25.0954831;
 var Range = 2500;
 
+//=============================================================================================
+// Assemble request URL
 var urlBase = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 var urlOptions = {
     location: [Latitude, Longitude].join(','),
@@ -20,3 +25,40 @@ urlPostfix = urlPostfix.join('&');
 
 var url = urlBase + '?' + urlPostfix;
 console.log('Request URL is', url);
+
+//=============================================================================================
+// GET from Google
+console.log('Sending request... ');
+
+https.get(url, function(res) {
+    if(res.statusCode != 200) {
+        console.log('Request not OK with code', res.statusCode);
+        return false;
+    }
+
+    res.setEncoding('utf8');
+    var data = '';
+    var req = this;
+
+    var i = 0;
+    res.on('data', (chunk) => {
+        data += chunk;
+
+        i += 1;
+        process.stdout.write('Receiving' + '.'.repeat(i) + '\r');
+    });
+    res.on('end', () => {
+        try {
+            data = JSON.parse(data);
+        }
+        catch(e) {
+            console.log('Couldn\'t parse data as JSON');
+            console.log(e.message);
+            return false;
+        }
+        req.emit('done', data);
+    });
+}).on('done', (data) => {
+    console.log('Request complete');
+    console.log(JSON.stringify(data, undefined, 4));
+});
